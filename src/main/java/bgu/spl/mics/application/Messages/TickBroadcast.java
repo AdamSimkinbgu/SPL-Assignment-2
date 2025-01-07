f5560d721687807d2167aca6db854e53137d2843
Adam Simkin <simkinad@post.bgu.ac.il>
2025-01-07 22:48:50 +0200

    Actually adding the messages
    git betrayed me ;(

---
+++ src/main/java/bgu/spl/mics/application/Messages/TickBroadcast.java
@@ -0,0 +1,7 @@
+package bgu.spl.mics.application.Messages;
+
+import bgu.spl.mics.Broadcast;
+
+public class TickBroadcast implements Broadcast {
+
+}
---
+++ src/main/java/bgu/spl/mics/application/Messages/TrackedObjectsEvent.java
@@ -0,0 +1,7 @@
+package bgu.spl.mics.application.Messages;
+
+import bgu.spl.mics.Event;
+
+public class TrackedObjectsEvent implements Event<Boolean> {
+
+}
